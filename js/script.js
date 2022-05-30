4bb9406945f46a5975118e054a0d4a1574e09d2b
Mohammad <mohammadimaan484@gmail.com>
2022-05-30 05:14:43 +0500

    Created basic UI

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@ const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
+
+const operate = (x, y, operator) => {
+    switch(operator) {
+        case "+":
+            return add(x, y);
+        case "-":
+            return subtract(x, y);
+        case "*":
+            return multiply(x, y);
+        case "/":
+            return divide(x, y);
+    }
+};
