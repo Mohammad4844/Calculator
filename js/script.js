67f2d70dfd987a8786373549cb21e8b2ccb855d5
Mohammad <mohammadimaan484@gmail.com>
2022-05-30 10:10:49 +0500

    Added DEL and CLR functionality, fixed few corner case issues

--- js/script.js
+++ js/script.js
@@ -8,31 +8,48 @@ operatorButtons.forEach(button => button.addEventListener("click", () => {
     let operator = button.textContent;
     // When someone clicks an operator before entering a number
     if (displayValue === "" && currentOperator === "" && value1 === null) throw "Enter a number before selecting an operator";
-    if (currentOperator === "") {
+    if (currentOperator === "" || currentOperator === "=") {
         if (value1 === null) 
             value1 = Number.parseFloat(displayValue); 
-        currentOperator = operator;
      } 
-     else if (displayValue === "") { // Case for changing the operator
-         currentOperator = operator;
-     }
-     else { // Default case
+     else if (displayValue !== "") { // Default case
         value2 = Number.parseFloat(displayValue);
-        let ans = operate(value1, value2, currentOperator);
-        document.querySelector(".display").textContent = value1 = ans;
-        currentOperator = (operator === "=") ? "" : operator;
+        try {
+            let ans = operate(value1, value2, currentOperator);
+            outputToDisplay(Math.round((value1 = ans) * 10000) / 10000);
+        } catch (err) {
+            alert(err);
+            clearButton.click();
+        }
      }
+     currentOperator = operator;
      displayValue = "";
 }));
 
 let buttons = document.querySelectorAll(".number");
-buttons.forEach(button => button.addEventListener("click", () => updateDisplay(button.textContent)));
+buttons.forEach(button => button.addEventListener("click", () => outputToDisplay(displayValue += button.textContent)));
 
+let clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", () => {
+    displayValue = currentOperator = "";
+    value1 = value2 = null;
+    outputToDisplay("");
+});
+
+let deleteButton = document.querySelector(".delete");
+deleteButton.addEventListener("click", () => {
+    if (displayValue === "") return;
+    displayValue = displayValue.substring(0, displayValue.length - 1);
+    outputToDisplay(displayValue);
+});
 
 const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
-const divide = (x, y) => x / y;
+const divide = (x, y) => {
+    if (y === 0) throw "You can't divide by 0!";
+    return x / y;
+};
 
 const operate = (x, y, operator) => {
     switch(operator) {
@@ -47,6 +64,4 @@ const operate = (x, y, operator) => {
     }
 };
 
-const updateDisplay = (x) => {
-    document.querySelector(".display").textContent = displayValue += x;
-};
+const outputToDisplay = x => document.querySelector(".display").textContent = x;
